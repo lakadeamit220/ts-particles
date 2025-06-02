1c1429985e945b70845149b92cf769efab116b47
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-02 17:03:32 +0530

    Added ISRODemo Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import BigCircles from "./components/BigCircles";
 import Fireworks from "./components/Fireworks";
+import ISRODemo from "./components/ISRO";
 import TsParticlesDemo from "./components/TSParticlesDemo";
 
 function App() {
@@ -7,7 +8,8 @@ function App() {
     <>
       {/* <TsParticlesDemo /> */}
       {/* <BigCircles/> */}
-      <Fireworks />
+      {/* <Fireworks /> */}
+      <ISRODemo/>
     </>
   );
 }
